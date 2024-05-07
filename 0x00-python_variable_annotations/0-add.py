ae5d29981cc8b79e51630090a191cb01da72a279
Samuel Muchoki <njorogemuchoki15@gmail.com>
2024-05-07 14:29:57 +0300

    Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,7 @@
 
 """Basic annotations -add module"""
 
+
 def add(a: float, b: float) -> float:
     """Function add that takes a float a and a float b as argumuments
     and returns their sum as a float
