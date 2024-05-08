8b190e5f7c5242c91a0ee049e34b3873e6559787
Samuel Muchoki <njorogemuchoki15@gmail.com>
2024-05-08 09:45:53 +0300

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Basic annotations - floor"""
+
+
+def floor(n: float) -> int:
+    """
+    A type-annotated function floow which takes a float n as argument and
+    returns the floor of the float.
+    """
+
+    return int(n)
